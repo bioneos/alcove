4f576e66620119bf5d9a21a8e21887dda8ff25fc
Gautam <gautamsharda001@gmail.com>
2021-06-19 18:37:43 -0500

    made changes recommended by Dominic in a previous PR addressing issue 77 (which is now closed)

--- public/js/machine.js
+++ public/js/machine.js
@@ -1,1 +1,34 @@
-$('#machine-table').tablesort();
+// $('#machine-table').tablesort();
+
+function generateCalendar(calendar) {
+    let eventDates = [];
+    console.log(calendar[3])
+    for (let i = 0; i < calendar[0].length; i++)
+    {
+      let className;
+      if (calendar[0][i] !== 'false') 
+      { 
+        if (calendar[2][i] !== '') className = 'positive';
+        else className = 'negative machine-dashboard-link-non-selectable';
+        if (calendar[1][i] === 'true') className += ' machine-dashboard-calendar-today-border'
+      }
+      else 
+      {
+        className = 'machine-dashboard-disabled'
+        if (calendar[1][i] === 'true') className += ' machine-dashboard-calendar-today-border'
+      }
+      eventDates.push({date: new Date(calendar[4][i]), class: className});
+    }
+    $('#inline_calendar')
+      .calendar({
+        eventDates: eventDates,
+        disableMinute: true,
+        type: 'date',
+        onSelect : function(date, model) {
+          date = String(date)
+          filtered_array = calendar[4].filter(calendar_dates => calendar_dates.includes(date.substring(0, date.indexOf(2020) + 4)))
+          window.location = '#backupevent-' + calendar[3][calendar[4].indexOf(filtered_array[0]) + 1]
+        }
+      });
+  }
+  
