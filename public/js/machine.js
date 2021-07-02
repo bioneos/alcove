ee09cf1037459f17a0d513587bc8ce8914a83d6a
Gautam <gautamsharda001@gmail.com>
2021-07-02 12:23:35 -0500

    syntax-related changes to implementation of fomantic-ui calendar

--- public/js/machine.js
+++ public/js/machine.js
@@ -1,18 +1,19 @@
 // $('#machine-table').tablesort();
 function generateCalendar(calendar) {
   let eventDates = [];
-  for (let i = 0; i < calendar.length; i++){
+  for (let i = 0; i < calendar.length; i++)
+  {
     let className;
     if (calendar[i].bucket !== 'false')
     {
       if (calendar[i].backup !== '') className = 'positive';
       else className = 'negative machine-dashboard-link-non-selectable';
-      if (calendar[i].today === 'true') className += ' machine-dashboard-calendar-today-border'
+      if (calendar[i].today === 'true') className += ' machine-dashboard-calendar-today-border';
     }
     else
     {
       className = 'machine-dashboard-disabled'
-      if (calendar[i].today === 'true') className += 'machine-dashboard-calendar-today-border'
+      if (calendar[i].today === 'true') className += 'machine-dashboard-calendar-today-border';
     }
     eventDates.push({date: new Date(calendar[i].date), class: className});
   }
